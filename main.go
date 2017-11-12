bdce8a17cc316238841f90c93caf98bd9c95dfad
Caeseason <757841203@qq.com>
2017-11-12 19:14:02 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ import (
 )
 
 func openconf(s string) string { //读取本地文件
-	z, err := os.Open("cookie.conf")
+	z, err := os.Open(s)
 	if err != nil {
 		log.Println("没有cookie.conf文件！")
 	}
-	buf := make([]byte, 1024)
+	buf := make([]byte, 10240)
 	n, _ := z.Read(buf)
 	cookie := string(buf[:n])
 	defer z.Close()
@@ -39,22 +39,24 @@ func date(cookie string) string {
 }
 func main() {
 	var cookie string
-	cookie0 := openconf("cookie.conf")                           //读取本地conf
-	cookie1 := regexp0(`\"(tieba|bilibili|smzdm)\"=.+`, cookie0) //func用regexp0,传递cookie里的每一行
-
-	cookie = regexp1(`(\"tieba\"=|\r)`, cookie1[0])
+	cookie0 := openconf("cookie.conf") //读取本地conf
+	c0 := regexp0("\"tieba\"=(.+|)", cookie0)
+	c1 := regexp0("\"bilibili\"=(.+|)", cookie0)
+	c2 := regexp0("\"smzdm\"=(.+|)", cookie0)
+	cookie = regexp1(`(\"tieba\"=|\r)`, c0[0])
 	if cookie != "" {
 		tieba.Tieba(cookie) //把贴吧cookie传递到GO/qiandao/tieba.go包使用
 		cookie = date(cookie)
 	}
 
-	cookie = regexp1("(\"bilibili\"=|\r)", cookie1[1])
+	cookie = regexp1("(\"bilibili\"=|\r)", c1[0])
 	if cookie != "" {
 		bilibili.Bilibili(cookie) //把贴吧cookie传递到GO/qiandao/bilibili.go包使用
 		cookie = date(cookie)
 	}
-	cookie = regexp1("(\"smzdm\"=|\r)", cookie1[2])
+	cookie = regexp1("(\"smzdm\"=|\r)", c2[0])
 	if cookie != "" {
+
 		smzdm.SMZDM(cookie) //把贴吧cookie传递到GO/qiandao/smzdm.go包使用
 		cookie = date(cookie)
 	}
