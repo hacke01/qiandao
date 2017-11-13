fd1dd01f3a5745a903620a1c783324194717fdf1
Caeseason <757841203@qq.com>
2017-11-13 11:53:37 +0800

    Create qiandao-go.sh

---
+++ v1.1/qiandao-go.sh
@@ -0,0 +1,7 @@
+mkdir /koolshare/qiandao;
+cd /koolshare/qiandao;
+wget https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.1/qiandao-arm;
+wget https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.1/qiandao。sh;
+wget https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.1/cookie.conf;
+chmod 755 /koolshare/qiandao/qiandao-arm;
+chmod 755 /koolshare/qiandao/qiandao.sh;
