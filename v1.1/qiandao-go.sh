0d525cf0c474e5c10311286e38716cfc5e52c2b6
Caeseason <757841203@qq.com>
2017-11-13 15:30:45 +0800

    Delete qiandao-go.sh

--- v1.1/qiandao-go.sh
+++
@@ -1,7 +0,0 @@
-mkdir /koolshare/qiandao;
-cd /koolshare/qiandao;
-wget -O qiandao-arm https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.1/qiandao-arm;
-wget -O qiandao.sh https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.1/qiandao.sh;
-wget -O cookie.conf https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.1/cookie.conf;
-chmod 755 /koolshare/qiandao/qiandao-arm;
-chmod 755 /koolshare/qiandao/qiandao.sh;
