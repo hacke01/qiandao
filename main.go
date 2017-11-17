44e763995ad7f5637a6d27df321dc87a24aa00d3
Caeseason <757841203@qq.com>
2017-11-17 23:51:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"github.com/Caeseason/qiandao/qiandao/bilibili"
-	"github.com/Caeseason/qiandao/qiandao/smzdm"
-	"github.com/Caeseason/qiandao/qiandao/tieba"
-
+	"GO/qiandao/bilibili"
+	"GO/qiandao/hostloc"
+	"GO/qiandao/smzdm"
+	"GO/qiandao/tieba"
+	"GO/qiandao/v2ex"
+	"io/ioutil"
 	"log"
 	"os"
 	"regexp"
-	"time"
 )
 
 func openconf(s string) string { //读取本地文件
-	z, err := os.Open(s)
+	fi, err := os.Open(s)
 	if err != nil {
-		log.Println("没有cookie.conf文件！")
+		log.Println("没有这个文件！")
 	}
-	buf := make([]byte, 10240)
-	n, _ := z.Read(buf)
-	cookie := string(buf[:n])
-	defer z.Close()
-	return cookie
+	defer fi.Close()
+	fd, err := ioutil.ReadAll(fi)
+	return string(fd)
 }
 func regexp0(s, v string) []string { //正则寻找字符串
 	reg := regexp.MustCompile(s)
 	regs := reg.FindAllString(v, -1)
+
 	return regs
 }
 func regexp1(s, v string) string { //正则删除字符
@@ -32,33 +32,43 @@ func regexp1(s, v string) string { //正则删除字符
 	regs := reg.ReplaceAllString(v, "")
 	return regs
 }
-func date(cookie string) string {
-	cookie = ""
-	time.Sleep(1 * time.Second)
-	return cookie
-}
+
 func main() {
 	var cookie string
 	cookie0 := openconf("cookie.conf") //读取本地conf
-	c0 := regexp0("\"tieba\"=(.+|)", cookie0)
-	c1 := regexp0("\"bilibili\"=(.+|)", cookie0)
-	c2 := regexp0("\"smzdm\"=(.+|)", cookie0)
-	cookie = regexp1(`(\"tieba\"=|\r)`, c0[0])
+
+	c0 := regexp0(`"tieba"=.+`, cookie0)
+	c1 := regexp0(`"bilibili"=.+`, cookie0)
+	c2 := regexp0(`"smzdm"=.+`, cookie0)
+	c3 := regexp0(`"hostloc"=.+`, cookie0)
+	c4 := regexp0(`"v2ex"=.+`, cookie0)
+
+	cookie = regexp1(`("tieba"=|\r)`, c0[0])
 	if cookie != "" {
 		tieba.Tieba(cookie) //把贴吧cookie传递到GO/qiandao/tieba.go包使用
-		cookie = date(cookie)
+		cookie = ""
 	}
-
-	cookie = regexp1("(\"bilibili\"=|\r)", c1[0])
+	cookie = regexp1(`("bilibili"=|\r)`, c1[0])
 	if cookie != "" {
 		bilibili.Bilibili(cookie) //把贴吧cookie传递到GO/qiandao/bilibili.go包使用
-		cookie = date(cookie)
+		cookie = ""
 	}
-	cookie = regexp1("(\"smzdm\"=|\r)", c2[0])
+	cookie = regexp1(`("smzdm"=|\r)`, c2[0])
+	if cookie != "" {
+		smzdm.SMZDM(cookie) //把贴吧cookie传递到GO/qiandao/smzdm.go包使用
+		cookie = ""
+	}
+	cookie = regexp1(`("hostloc"=|\r)`, c3[0])
 	if cookie != "" {
 
-		smzdm.SMZDM(cookie) //把贴吧cookie传递到GO/qiandao/smzdm.go包使用
-		cookie = date(cookie)
+		hostloc.Hostloc(cookie) //把贴吧cookie传递到GO/qiandao/hostloc.go包使用
+		cookie = ""
+	}
+	cookie = regexp1(`("v2ex"=|\r)`, c4[0])
+	if cookie != "" {
+
+		v2ex.V2ex(cookie) //把贴吧cookie传递到GO/qiandao/v2ex.go包使用
+		cookie = ""
 	}
 
 }
