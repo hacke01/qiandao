14d698b13d032a1281c62da0ffbda8e2c53b28fe
Caeseason <757841203@qq.com>
2017-11-18 20:31:32 +0800

    Update qiandao.sh

--- qiandao.sh
+++ qiandao.sh
@@ -1,8 +1,8 @@
 mkdir /koolshare/qiandao;
 cd /koolshare/qiandao;
-wget -O qiandao-arm https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.2/qiandao-arm;
-wget -O qiandao.sh https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.2/qiandao.sh;
-wget -O cookie.conf https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.2/cookie.conf;
+wget -O qiandao-arm https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.3/qiandao-arm;
+wget -O qiandao.sh https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.3/qiandao.sh;
+wget -O cookie.conf https://raw.githubusercontent.com/Caeseason/qiandao/master/v1.3/cookie.conf;
 chmod 755 /koolshare/qiandao/qiandao-arm;
 chmod 755 /koolshare/qiandao/qiandao.sh;
 cd /jffs/scripts
